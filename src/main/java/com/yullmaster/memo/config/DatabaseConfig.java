496ae9e44d993267523e6b04bc642cc0c20254c5
DESKTOP-6KBPV3D\user <syr96@naver.com>
2022-01-21 20:22:33 +0900

    -memo 프로젝트 생성 및 최초 업로드 -기본 설정만 해놓은 상태

---
+++ src/main/java/com/yullmaster/memo/config/DatabaseConfig.java
@@ -0,0 +1,22 @@
+package com.yullmaster.memo.config;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+public class DatabaseConfig {
+	@Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+
+        Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
+        sessionFactory.setMapperLocations(res);
+
+        return sessionFactory.getObject();
+	}
+}
